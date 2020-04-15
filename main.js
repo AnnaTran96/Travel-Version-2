3866897ad90f60ffa030cbf601e538bca6790acd
Anna Tran <annatran@annas-air.mynet>
2020-04-15 20:25:35 +0100

    Final changes

--- main.js
+++ main.js
@@ -49,3 +49,13 @@ function sidebarScroll() {
       }
     }
 }
+
+function disableScrolling(){
+  var x=window.scrollX;
+  var y=window.scrollY;
+  window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+  window.onscroll=function(){};
+}
